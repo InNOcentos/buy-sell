7926bcbdbc14314de66f4228959ef319457845d4
Boris <smrnvbrs@gmail.com>
2020-08-23 17:58:31 +0300

    added pug templates from markup,service-server express refactor

--- src/express/index.js
+++ src/express/index.js
@@ -5,11 +5,15 @@ const express = require(`express`);
 const mainRoutes = require('./routes/main-routes');
 const offersRoutes = require('./routes/offers-routes');
 const myRoutes = require('./routes/my-routes');
-
-const  DEFAULT_PORT = 8080;
-
+const path = require(`path`);
+const PUBLIC_DIR = `public`;
+const DEFAULT_PORT = 8080;
 const app = express();
 
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use('/',mainRoutes);
 app.use('/offers',offersRoutes);
 app.use('/my',myRoutes);
