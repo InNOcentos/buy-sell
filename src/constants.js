6c5f1d3e59d3a12fc41ed1e2a6faeaa072b8b6bb
Boris <smrnvbrs@gmail.com>
2020-08-25 17:49:24 +0300

    added id and comments lines to mocks with nanoid,added interaction to rest api with mocks(CRUD), removed markup

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 'use strict';
 
+module.exports.API_PREFIX = `/api`;
+module.exports.MAX_ID_LENGTH = 6;
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.USER_ARGV_INDEX = 2;
 module.exports.ExitCode = {
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,23 +2,29 @@
 
 const fs = require(`fs`).promises;
 const chalk = require('chalk');
+const {nanoid} = require(`nanoid`);
+const {MAX_ID_LENGTH} = require('../../constants');
 
 const {
   getRandomInt,
   shuffle
 } = require('../../utils');
 
-const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
 const OfferType = {
   OFFER: `offer`,
   SALE: `sale`,
 };
+
+const DEFAULT_COUNT = 1;
+const MAX_COMMENTS = 4;
+
 const SumRestrict = {
   MIN: 1000,
   MAX: 100000,
@@ -28,6 +34,8 @@ const PictureRestrict = {
   MIN: 1,
   MAX: 16,
 };
+
+
 
 const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
 
@@ -41,14 +49,25 @@ const readContent = async (filePath) => {
   }
 };
 
-const generateOffers = (count, titles, categories, sentences) => (
+const generateComments = (count, comments) => (
   Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `),
+  }))
+);
+
+const generateOffers = (count, titles, categories, sentences, comments) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
     category: [categories[getRandomInt(0, categories.length - 1)]],
     description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+    comments: generateComments(getRandomInt(1,MAX_COMMENTS),comments)
   }))
 );
 
@@ -58,10 +77,11 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences, comments));
 
     try {
       await fs.writeFile(FILE_NAME,content);
