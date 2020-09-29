4b2199568b27ec69bdeac971bfcd58c9cd821938
Boris <smrnvbrs@gmail.com>
2020-09-29 16:43:15 +0300

    added file type check for multer

--- src/express/multer/index.js
+++ src/express/multer/index.js
@@ -11,20 +11,36 @@ const storage = multer.diskStorage({
         next(null,file.originalname);
     },
 });
-const upload = multer({ storage: storage }).single(`avatar`);
+
+const fileFilter = (req,file,next)=> {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/webp") {
+        next(null, true);
+      } else {
+        next(null, false);
+        return next(new Error('Only .png, .jpg , .jpeg and webp format allowed!'));
+      }
+} 
+
+const upload = multer({ storage: storage,fileFilter: fileFilter }).single(`avatar`);
 
 exports.uploadFile = async (req,res,next) => {
     
     upload(req,res,function (err) {
-        if (err) {
-            console.log(err.message)
-        }
-        if (!fileName.name) {
-            next();
-            return;
-        }
-        req.body.avatar = fileName.name.originalname;
-        next(); 
+        try {
+            if (err) {
+                console.log(err.message)
+                return res.send(err.message)
+            } 
+            if (!fileName.name) {
+                next();
+                return;
+            }
+            req.body.avatar = fileName.name.originalname;
+            next(); 
+        }catch (err) {
+            console.log(`${err.message}`)
+            next(err)
+        }   
     })
 };
 
